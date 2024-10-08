9d6fa30ff8c88be7f0b032c5420407f73f464ded
ProsperAgada <ProsperAgada>
2024-10-08 10:52:10 +0100

    updated api

--- src/main/java/com/api/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/api/employeemanagement/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.api.employeemanagement.controller;
 
 import com.api.employeemanagement.dto.EmployeeDto;
 import com.api.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,46 +11,48 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @author Paul Nyishar
- * Web Applications Developer
- * @Date 24/07/2022
- */
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
+
+    @Value("${frontend.ip}") // Inject the IP address from application properties or environment
+    private String frontendIp;
+
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
+    @CrossOrigin(origins = "${frontend.ip}") // Use the injected IP for CORS
     @PostMapping("employees")
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         return new ResponseEntity<>(employeeService.createEmployee(employeeDto), HttpStatus.CREATED);
     }
 
+    @CrossOrigin(origins = "${frontend.ip}")
     @GetMapping("employees")
     public List<EmployeeDto> getAllEmployees(){
         return employeeService.getAllEmployees();
     }
 
+    @CrossOrigin(origins = "${frontend.ip}")
     @DeleteMapping("employees/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
-        boolean deleted = false;
-        deleted = employeeService.deleteEmployee(id);
+        boolean deleted = employeeService.deleteEmployee(id);
         Map<String, Boolean> response = new HashMap<>();
         response.put("Deleted", deleted);
         return ResponseEntity.ok(response);
     }
 
+    @CrossOrigin(origins = "${frontend.ip}")
     @GetMapping("employees/{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id){
         EmployeeDto employeeDto = employeeService.getEmployeeById(id);
         return ResponseEntity.ok(employeeDto);
     }
 
+    @CrossOrigin(origins = "${frontend.ip}")
     @PutMapping("employees/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDto){
         employeeDto = employeeService.updateEmployee(id, employeeDto);
